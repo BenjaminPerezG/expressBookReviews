0e904753f58d3049c5eadbb92551ba3c5fbccadb
Benjamín García <144961829+BenjaminPerezG@users.noreply.github.com>
2024-06-27 02:47:16 -0600

    initial commit

--- final_project/index.js
+++ final_project/index.js
@@ -5,18 +5,26 @@ const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
 const app = express();
+const secretKey = "secure_key"; // this can be replaced with a more secure key
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer", session({ secret: "fingerprint_customer", resave: true, saveUninitialized: true }))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req, res, next) {
+  const token = req.headers.authorization;
+  jwt.verify(token, secretKey, (err, decoded) => {
+    {
+      req.user = decoded;
+      next();
+    }
+  });
 });
- 
-const PORT =5000;
+
+
+const PORT = 3000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running on port " + PORT));
